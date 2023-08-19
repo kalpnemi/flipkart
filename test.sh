b5f1a5b784af2c028cee01bf2350e025c12ace67
raushan <raushan1@gmail.com>
2023-08-19 21:35:43 +0530

    message

--- test.sh
+++ test.sh
@@ -1,1 +1,12 @@
 this shell scripting
+
+new file
+-----------
+working a > stagging a > local repo
+git add .             git commit -m "message" "
+
+
+old file
+--------------
+working a > stagging > local repo
+   git commit -a -m "message"
