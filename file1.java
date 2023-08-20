0ca4db31e771cb126c2b0c678d1c7f945eeef8f0
raushan <raushan1@gmail.com>
2023-08-20 10:24:22 +0530

    upding in master

--- file1.java
+++ file1.java
@@ -68,3 +68,6 @@ now developer code avaible here
 
 ---------------------------------------------
 hi i am developing code in dev branch to deploy new functionality.
+----------------------------------------------------------------------
+upading the code in master branch 
+----------------------------------------
